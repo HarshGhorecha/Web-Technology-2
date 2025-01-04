a0f36684203dc7118c817ca76230d1a3c88d68a3
Harsh Ghorecha <harshgp5123@gmail.com>
2025-01-04 12:34:17 +0530

    Create child_process.js

---
+++ child_process.js
@@ -0,0 +1,18 @@
+const {exec} = require('child_process');
+
+exec (' echo HELLO!!!... Mr.HARSH', (error, stdout, stderr) =>{
+    if(error)
+    {
+        console.log(`Error: ${error.message}`);
+        return;
+    }
+    if(stderr)
+    {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    else
+    {
+        console.log(`Output: ${stdout} `);
+    }
+})
