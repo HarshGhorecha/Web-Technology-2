f13341651e9062cd95448cc3739bd74ddf4851ad
Harsh Ghorecha <harshgp5123@gmail.com>
2025-01-04 12:35:58 +0530

    child_process.js Node JS Core Modules

--- child_process.js
+++ child_process.js
@@ -1,6 +1,6 @@
 const {exec} = require('child_process');
 
-exec (' echo HELLO!!!... Mr.HARSH', (error, stdout, stderr) =>{
+exec (' echo HELLO!!!... Mr.HARSH', (error, stdout, stderr) =>{ 
     if(error)
     {
         console.log(`Error: ${error.message}`);
